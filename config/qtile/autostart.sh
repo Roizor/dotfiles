826ce836139dff7511359ff8f1fb38eea79aaea3
Roizor <aidenjs9@icloud.com>
2023-06-10 14:25:15 +0000

    do the mabobber

--- config/qtile/autostart.sh
+++ config/qtile/autostart.sh
@@ -1,1 +1,3 @@
-qtile/config.py
+xrandr --output $(xrandr | grep con | grep HDMI | cut -d' ' -f 1) --mode 2560x1440 --left-of $(xrandr | grep con | grep eDP | cut -d' ' -f 1)
+nitrogen --restore
+picom -b
